9abee754f06d1193b9ed9fec04e202f27644d0d7
libing618 <997098144@qq.com>
2018-06-26 23:45:03 +0800

    1806029

--- libs/accessLib.js
+++ libs/accessLib.js
@@ -1,5 +1,4 @@
 const URL_base = 'https://trce3aqb.engine.lncld.net/1.1/functions/';
-const wxappNumber = 2;    //本小程序在开放平台中自定义的序号
 const COS = require('../libs/cos-wx-sdk-v5')
 var cos = new COS({
   getAuthorization: function (params, callback) {//获取签名 必填参数
@@ -22,6 +21,22 @@ function lcRequest(f,parmart){
         'X-LC-Key': 'CBbIFAhL4zOyCT9PQM5273bP',
         'Accept': 'application/json'                 //这有一个大坑，不能用Content-Type：'application/json'
       },
+      data: parmart,
+      success: function(res){     //这要注意返回的json名称有变化，要在控制台进行查看,千万不要用id这样的保留字作自定义的列名
+        resolve(res);
+      },
+      fail: function(error){
+        reject(error);
+      }
+    })
+  })
+};
+function scfRequest(fUrl,header,parmart){
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: fUrl,
+      method: 'GET',
+      header: header,
       data: parmart,
       success: function(res){     //这要注意返回的json名称有变化，要在控制台进行查看,千万不要用id这样的保留字作自定义的列名
         resolve(res);
--- model/accessData.js
+++ model/accessData.js
@@ -1,26 +1,7 @@
 const wxappNumber = 2;    //本小程序在开放平台中自定义的序号
-
+const {lcRequest,cosUploadFile} = require('../libs/accessLib')
 var app = getApp();
-function lcRequest(f,parmart){
-  return new Promise((resolve, reject) => {
-    wx.request({
-      url: URL_base+f,
-      method: 'POST',
-      header: {
-        'X-LC-Id': 'Trce3aqbc6spacl6TjA1pndr-gzGzoHsz',
-        'X-LC-Key': 'CBbIFAhL4zOyCT9PQM5273bP',
-        'Accept': 'application/json'                 //这有一个大坑，不能用Content-Type：'application/json'
-      },
-      data: parmart,
-      success: function(res){     //这要注意返回的json名称有变化，要在控制台进行查看,千万不要用id这样的保留字作自定义的列名
-        resolve(res);
-      },
-      fail: function(error){
-        reject(error);
-      }
-    })
-  })
-}
+
 module.exports = {
   openWxLogin: function() {              //取无登录状态数据
     return new Promise((resolve, reject) => {
