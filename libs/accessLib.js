65c8920b490247b6041f935a6e261f9fbc00376f
libing618 <997098144@qq.com>
2018-06-28 18:17:53 +0800

    1806031

--- libs/accessLib.js
+++ libs/accessLib.js
@@ -32,12 +32,12 @@ function lcRequest(f,parmart){
     })
   })
 };
-function scfRequest(fUrl,header,parmart){
+function scfRequest(fUrl,parmart){
   return new Promise((resolve, reject) => {
     wx.request({
       url: fUrl,
-      method: 'GET',
-      header: header,
+      method: 'POST',
+      header: {content-type：'application/json'},
       data: parmart,
       success: function(res){     //这要注意返回的json名称有变化，要在控制台进行查看,千万不要用id这样的保留字作自定义的列名
         resolve(res);
